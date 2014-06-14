df5d13ecf6a548eaa9b5916d8ffe52553c3023bb
Greg Boyington <greg@automagick.us>
2014-06-14 09:43:39 -0400

    fixing wsgi settings for production

--- mmsn/wsgi/__init__.py
+++ mmsn/wsgi/__init__.py
@@ -1,7 +1,8 @@
 import socket
-(h, domain) = socket.gethostname().split('.', 2)
+h = socket.gethostname()
 try:
+    (h, domain) = socket.gethostname().split('.', 2)
     exec("from mmsn.wsgi.{0} import *".format(h)) in locals()
 except Exception as e:
     print "WARNING: Could not locate wgsi config for host '%s': %s" % (h, e)
-    from deploy import *
+    from production import *
