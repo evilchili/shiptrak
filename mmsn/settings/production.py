9dcf34748048e63a71555112b45e9b43277e04ec
Greg Boyington <greg@automagick.us>
2014-06-14 20:37:53 -0400

    Revert "switching to sqlite3"
    This reverts commit a7808a633116a5c373a3d4de05f0497437d0dc33.

--- mmsn/settings/production.py
+++ mmsn/settings/production.py
@@ -1,22 +1,12 @@
 import os
+import dj_database_url
 
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
-
-# For a heroku-hosted database
-#
-#import dj_database_url
-#DATABASES = {
-#    'default':  dj_database_url.config()
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':  dj_database_url.config()
 }
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyDHRIu1CdX0O95_bTdyyiom4Z84uzKG0bw'
