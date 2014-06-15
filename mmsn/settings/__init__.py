ea4bf4a0d1885db3da00060f3961882909162e60
Greg Boyington <greg@automagick.us>
2014-06-14 20:58:59 -0400

    removing staticfiles_dirs setting

--- mmsn/settings/__init__.py
+++ mmsn/settings/__init__.py
@@ -77,11 +77,9 @@ USE_TZ = True
 import os
 
 STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = ()
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django.contrib.staticfiles.finders.FileSystemFinder',
