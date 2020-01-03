a31388bab03709e77a3b0709bf855c4da36d7cf2
Greg Boyington <greg@automagick.us>
2020-01-03 11:17:24 -0800

    merging wsgi production config into defaults

--- mmsn/wsgi/__init__.py
+++ mmsn/wsgi/__init__.py
@@ -1,8 +1,13 @@
 import socket
+import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 h = socket.gethostname()
 try:
     (h, domain) = socket.gethostname().split('.', 2)
     exec(("from mmsn.wsgi.{0} import *".format(h)), locals())
 except Exception as e:
     print("WARNING: Could not locate wgsi config for host '%s': %s" % (h, e))
-    from .production import *
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmsn.settings")
+    application = Cling(get_wsgi_application())
