d7c6289226238efbed00672daa39cac101d9a7e8
Greg Boyington <greg@automagick.us>
2020-01-03 11:21:05 -0800

    unifying wsgi config

--- mmsn/wsgi/__init__.py
+++ mmsn/wsgi/__init__.py
@@ -1,13 +1,6 @@
-import socket
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-h = socket.gethostname()
-try:
-    (h, domain) = socket.gethostname().split('.', 2)
-    exec(("from mmsn.wsgi.{0} import *".format(h)), locals())
-except Exception as e:
-    print("WARNING: Could not locate wgsi config for host '%s': %s" % (h, e))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmsn.settings")
-    application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmsn.settings")
+application = Cling(get_wsgi_application())
